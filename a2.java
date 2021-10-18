9df370349937c18ced2375933f009cc09ab88803
Rupin-Oberoi <88402437+Rupin-Oberoi@users.noreply.github.com>
2021-10-19 00:23:25 +0530

    Add files via upload

--- a2.java
+++ a2.java
@@ -9,6 +9,8 @@ public class a2 {
     public static ArrayList<Assignment> asslist = new ArrayList<Assignment>();
     public static ArrayList<Assesment> assmlist = new ArrayList<Assesment>();
     public static ArrayList<Quiz> quizlist = new ArrayList<Quiz>();
+    public static ArrayList<Comment> commlist = new ArrayList<Comment>();
+    public static ArrayList<Submission> sublist = new ArrayList<Submission>();
     public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -24,6 +26,7 @@ public class a2 {
         System.out.println("2. Enter as student");
         System.out.println("3. Exit");
         sel1 = scan.nextInt();
+        @SuppressWarnings("unused")
         String misc = scan.nextLine();
         if (sel1==1){
             int len = instlist.size();
@@ -100,6 +103,15 @@ public class a2 {
                         System.out.println("ID: "+i+" "+assmlist.get(i).getQ()+" Max Marks: "+ assmlist.get(i).getMax()+"\n");
                     }
                 }
+                else if (sel3==7){
+                    for (int i=0;i<commlist.size();i++){
+                        System.out.println(commlist.get(i).getContent()+" - "+commlist.get(i).getUser().getName());
+                        System.out.println(dtf.format(commlist.get(i).getTime()));
+                    }
+                }
+                else if (sel3==8){
+                    i_user.putComment();
+                }
             }
         }
         else if (sel1==2){
@@ -131,12 +143,56 @@ public class a2 {
                         System.out.println("ID: "+i+" "+assmlist.get(i).getQ()+" Max Marks: "+ assmlist.get(i).getMax()+"\n");
                     }
                 }
+                else if (sel3==3){
+                    s_user.seePending();
+                    // System.out.print("Enter ID of assignment: ");
+                    // sel4 = scan.nextInt();
+                    // misc = scan.nextLine();
+                    // String file_name;
+                    // String extn;
+                    // if (assmlist.get(sel4).getType()=="Assignment"){
+                    //     System.out.print("Enter file name of submission: ");
+                    //     file_name = scan.nextLine();
+                    //     extn = file_name.substring(file_name.length()-4);
+                    //     while (!extn.equals(".zip")){
+                    //         System.out.print("Invalid extension. Try again: ");
+                    //         file_name = scan.nextLine();
+                    //         extn = file_name.substring(file_name.length()-4);
+                    //     }
+                    // }
+                    // else if (assmlist.get(sel4).getType()=="Quiz"){
+                    //     System.out.print(assmlist.get(sel4).getQ()+" ");
+                    //     file_name = scan.nextLine();
+                    // }
+
+                }
+                else if (sel3==5){
+                    for (int i=0;i<commlist.size();i++){
+                        System.out.println(commlist.get(i).getContent()+" - "+commlist.get(i).getUser().getName());
+                        System.out.println(dtf.format(commlist.get(i).getTime()));
+                    }
+                }
+                else if (sel3==6){
+                    s_user.putComment();
+                }
             }
         }
     }
 }   
 }
-class Instructor implements user{
+class Submission {
+    private Student doer;
+    private Instructor checker=null;
+    private String answer;
+    private int marks=0;
+    private Assesment problem;
+    public Submission(Student s, String a, Assesment as){
+        this.doer  =s;
+        this.answer = a;
+        this.problem = as;
+    }
+}
+class Instructor implements User{
     private String name;
     private int id;
     public Instructor(String n, int i){
@@ -155,6 +211,7 @@ class Instructor implements user{
         System.out.println("a"+sld_tpc);
         System.out.println("Enter number of slides");
         int n_sld = a2.scan.nextInt();
+        @SuppressWarnings("unused")
         String misc = a2.scan.nextLine();
         System.out.println("Enter content of slides");
         String sld_array[]  = new String[n_sld];
@@ -185,6 +242,7 @@ class Instructor implements user{
         String pst = a2.scan.nextLine();
         System.out.print("Enter max marks");
         int maxm = a2.scan.nextInt();
+        @SuppressWarnings("unused")
         String misc = a2.scan.nextLine();
         a2.asslist.add(new Assignment(this, pst, maxm));
         a2.assmlist.add(a2.asslist.get(a2.asslist.size()-1));
@@ -195,16 +253,71 @@ class Instructor implements user{
         a2.quizlist.add(new Quiz(this, ques));
         a2.assmlist.add(a2.quizlist.get(a2.quizlist.size()-1));
     }
+    public void putComment(){
+        System.out.println("Enter comment: ");
+        @SuppressWarnings("unused")
+        String misc= a2.scan.nextLine();
+        String comm = a2.scan.nextLine();
+        a2.commlist.add(new Comment(this,comm));
+    }
 }
-class Student implements user{
+class Student implements User{
     private String name;
+    private boolean[] sub_status;
     public Student(String n){
         this.name = n;
+        this.sub_status = new boolean[a2.assmlist.size()];
     }
     public String getName(){
         return this.name;
     }
-
+    public void putComment(){
+        System.out.print("Enter comment: ");
+        String comm = a2.scan.nextLine();
+        a2.commlist.add(new Comment(this,comm));
+    }
+    public void seePending(){
+        int count=0;
+        int sel4=-1;
+        String misc;
+        for (int i=0; i<a2.assmlist.size();i++){
+            if (!a2.assmlist.get(i).getSubmitters().contains(this) && a2.assmlist.get(i).getStatus()=="OPEN"){
+                count+=1;
+            }
+        }
+        if (count==0){
+            System.out.println("No pending assesments");
+        }
+        else{
+            System.out.println("Pending Assesments");
+            for (int i=0; i<a2.assmlist.size();i++){
+                if (!a2.assmlist.get(i).getSubmitters().contains(this) && a2.assmlist.get(i).getStatus()=="OPEN"){
+                    System.out.println("ID: "+i+" "+a2.assmlist.get(i).getQ()+" Max Marks: "+ a2.assmlist.get(i).getMax());
+            }
+        }
+            System.out.print("Enter ID of assignment: ");
+            sel4 = a2.scan.nextInt();
+            misc = a2.scan.nextLine();
+            String file_name=null;
+            String extn;
+            if (a2.assmlist.get(sel4).getType()=="Assignment"){
+            System.out.print("Enter file name of submission: ");
+            file_name = a2.scan.nextLine();
+            extn = file_name.substring(file_name.length()-4);
+            while (!extn.equals(".zip")){
+                System.out.print("Invalid extension. Try again: ");
+                file_name = a2.scan.nextLine();
+                extn = file_name.substring(file_name.length()-4);
+            }
+        }
+        else if (a2.assmlist.get(sel4).getType()=="Quiz"){
+        System.out.print(a2.assmlist.get(sel4).getQ()+" ");
+        file_name = a2.scan.nextLine();
+        }
+        a2.sublist.add(new Submission(this, file_name, a2.assmlist.get(sel4)));
+        a2.assmlist.get(sel4).getSubmitters().add(this);
+    }
+    }
 }
 class Video{
     private String name;
@@ -235,8 +348,27 @@ interface Classes{
     public void view();
 }
 class Comment{
-    
-    
+    private User user;
+    private LocalDateTime c_time;
+    private String content;
+    public Comment(User u, String c){
+        this.user = u;
+        this.c_time = LocalDateTime.now();
+        this.content = c;
+    }
+    public LocalDateTime getTime(){
+        return this.c_time;
+    }
+    public User getUser(){
+        return this.user;
+    }
+    public String getContent(){
+        return this.content;
+    }
+}
+interface User{
+    public String getName();
+    public void putComment();
 }
 class Slide{
     private String topic;
@@ -267,14 +399,13 @@ class Slide{
         return this.uploader;
     }
 }
-interface user{
-    public String getName();
-}
+
 class Assignment implements Assesment{
     private Instructor uploader;
     private String p_st;
     private int max_m;
     private String status="OPEN";
+    private ArrayList<Student> sub_list= new ArrayList<Student>();
     public Assignment(Instructor u, String st, int m){
         this.uploader = u;
         this.p_st = st;
@@ -297,17 +428,29 @@ class Assignment implements Assesment{
     public int getMax(){
         return this.max_m;
     }
+    public Instructor getUploader(){
+        return this.uploader;
+    }
+    public ArrayList<Student> getSubmitters(){
+        return this.sub_list;
+    }
+    public String getType(){
+        return "Assignment";
+    }
 }
 interface Assesment{
     public void setStatus(int a);
     public String getStatus();
     public String getQ();
     public int getMax();
-    
+    public ArrayList<Student> getSubmitters();
+    public String getType();
 }
 class Quiz implements Assesment{
     private Instructor uploader;
     private String p_st;
+    private ArrayList<Student> sub_list = new ArrayList<Student>(); 
+    @SuppressWarnings("unused")
     private int max_m=1;
     private String status="OPEN";
     public Quiz(Instructor u, String st){
@@ -330,5 +473,14 @@ class Quiz implements Assesment{
     }
     public int getMax(){
         return 1;
+    }
+    public Instructor getUploader(){
+        return this.uploader;
+    }
+    public ArrayList<Student> getSubmitters(){
+        return this.sub_list;
+    }
+    public String getType(){
+        return "Quiz";
     }
 }
